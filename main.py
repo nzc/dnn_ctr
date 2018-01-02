eec14c62d5b32de6dee7b2df227e5c8417d7330b
nzc <120446477@qq.com>
2018-01-02 13:21:29 +0800

    add tiny dat file

--- main.py
+++ main.py
@@ -8,6 +8,6 @@ result_dict = data_preprocess.read_criteo_data('./data/tiny_train_input.csv', '.
 test_dict = data_preprocess.read_criteo_data('./data/tiny_test_input.csv', './data/category_emb.csv')
 
 with torch.cuda.device(2):
-    deepfm = DeepFM.DeepFM(39,result_dict['feature_sizes'],verbose=True,use_cuda=True, weight_decay=0.0001,use_fm=False,use_ffm=True,use_deep=False).cuda()
-    deepfm.fit(result_dict['index'][0:2018], result_dict['value'][0:2018], result_dict['label'][0:2018],
-               test_dict['index'][0:2018], test_dict['value'][0:2018], test_dict['label'][0:2018],ealry_stopping=True,refit=True)
+    deepfm = DeepFM.DeepFM(39,result_dict['feature_sizes'],verbose=True,use_cuda=True, weight_decay=0.0001,use_fm=True,use_ffm=False,use_deep=True).cuda()
+    deepfm.fit(result_dict['index'], result_dict['value'], result_dict['label'],
+               test_dict['index'], test_dict['value'], test_dict['label'],ealry_stopping=True,refit=True)
